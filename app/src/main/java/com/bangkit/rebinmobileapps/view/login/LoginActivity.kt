cbb46601f82f37d04103db5fbfc907868c096a1f
Orry Frasetyo <orrytio12345@gmail.com>
2024-05-29 21:35:57 +0700

    [ory][add animation page login dan register]

--- app/src/main/java/com/bangkit/rebinmobileapps/view/login/LoginActivity.kt
+++ app/src/main/java/com/bangkit/rebinmobileapps/view/login/LoginActivity.kt
@@ -1,7 +1,10 @@
 package com.bangkit.rebinmobileapps.view.login
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
@@ -29,8 +32,41 @@ class LoginActivity : AppCompatActivity() {
             insets
         }
 
+        playAnimation()
+
         customTextEmail = binding.emailEdittext
         customTextPassword = binding.passwordEditText
+    }
+
+    private fun playAnimation() {
+        val title = ObjectAnimator.ofFloat(binding.loginTitleTextView, View.ALPHA, 1f).setDuration(100)
+        val emailTextView = ObjectAnimator.ofFloat(binding.emailTextView, View.ALPHA, 1f).setDuration(100)
+        val emailEditTextLayout = ObjectAnimator.ofFloat(binding.emailEditTextLayout, View.ALPHA, 1f).setDuration(100)
+        val passwordTextView = ObjectAnimator.ofFloat(binding.passwordTextView, View.ALPHA, 1f).setDuration(100)
+        val passwordEditTextLayout = ObjectAnimator.ofFloat(binding.passwordEditTextLayout, View.ALPHA, 1f).setDuration(100)
+        val forgot = ObjectAnimator.ofFloat(binding.forgotPasswordTextView, View.ALPHA, 1f).setDuration(100)
+        val btnLogin = ObjectAnimator.ofFloat(binding.loginButton, View.ALPHA, 1f).setDuration(100)
+        val or = ObjectAnimator.ofFloat(binding.orTextView, View.ALPHA, 1f).setDuration(100)
+        val btnLoginGoogle = ObjectAnimator.ofFloat(binding.buttonLoginGoogle, View.ALPHA, 1f).setDuration(100)
+        val dontAccountTextView = ObjectAnimator.ofFloat(binding.tvDontHaveAccount, View.ALPHA, 1f).setDuration(100)
+        val signUpTextView = ObjectAnimator.ofFloat(binding.tvSignUp, View.ALPHA, 1f).setDuration(100)
+
+        AnimatorSet().apply {
+            playSequentially(
+                title,
+                emailTextView,
+                emailEditTextLayout,
+                passwordTextView,
+                passwordEditTextLayout,
+                forgot,
+                btnLogin,
+                or,
+                btnLoginGoogle,
+                dontAccountTextView,
+                signUpTextView
+            )
+            startDelay = 100
+        }.start()
     }
 
 
--- app/src/main/java/com/bangkit/rebinmobileapps/view/signup/SignUpActivity.kt
+++ app/src/main/java/com/bangkit/rebinmobileapps/view/signup/SignUpActivity.kt
@@ -1,6 +1,9 @@
 package com.bangkit.rebinmobileapps.view.signup
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -26,7 +29,42 @@ class SignUpActivity : AppCompatActivity() {
             insets
         }
 
+        playAnimation()
+
         customTextEmail = binding.emailEdittext
         customTextPassword = binding.passwordEditText
+    }
+
+    private fun playAnimation() {
+        val title = ObjectAnimator.ofFloat(binding.signUpTitleTextView, View.ALPHA, 1f).setDuration(100)
+        val nameTextView = ObjectAnimator.ofFloat(binding.nameTextView, View.ALPHA, 1f).setDuration(100)
+        val nameEditTextLayout = ObjectAnimator.ofFloat(binding.nameEditTextLayout, View.ALPHA, 1f).setDuration(100)
+        val emailTextView = ObjectAnimator.ofFloat(binding.emailTextView, View.ALPHA, 1f).setDuration(100)
+        val emailEditTextLayout = ObjectAnimator.ofFloat(binding.emailEditTextLayout, View.ALPHA, 1f).setDuration(100)
+        val passwordTextView = ObjectAnimator.ofFloat(binding.passwordTextView, View.ALPHA, 1f).setDuration(100)
+        val passwordEditTextLayout = ObjectAnimator.ofFloat(binding.passwordEditTextLayout, View.ALPHA, 1f).setDuration(100)
+        val btnSignUp = ObjectAnimator.ofFloat(binding.signupButton, View.ALPHA, 1f).setDuration(100)
+        val or = ObjectAnimator.ofFloat(binding.orTextView, View.ALPHA, 1f).setDuration(100)
+        val btnSignUpGoogle = ObjectAnimator.ofFloat(binding.buttonSignupGoogle, View.ALPHA, 1f).setDuration(100)
+        val alreadyAccountTextView = ObjectAnimator.ofFloat(binding.tvAlreadyHaveAccount, View.ALPHA, 1f).setDuration(100)
+        val loginTextView = ObjectAnimator.ofFloat(binding.tvLogin, View.ALPHA, 1f).setDuration(100)
+
+        AnimatorSet().apply {
+            playSequentially(
+                title,
+                nameTextView,
+                nameEditTextLayout,
+                emailTextView,
+                emailEditTextLayout,
+                passwordTextView,
+                passwordEditTextLayout,
+                btnSignUp,
+                or,
+                btnSignUpGoogle,
+                alreadyAccountTextView,
+                loginTextView
+            )
+            startDelay = 100
+        }.start()
     }
 }
