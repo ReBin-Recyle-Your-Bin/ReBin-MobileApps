e7fcb34f1bbe956fc5643f259d9d6d428828c45e
Avrians <avriansyahb10@gmail.com>
2024-05-31 09:56:43 +0700

    [av][setting binding detection and tombol detection]

--- app/src/main/java/com/bangkit/rebinmobileapps/view/main/MainActivity.kt
+++ app/src/main/java/com/bangkit/rebinmobileapps/view/main/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.bangkit.rebinmobileapps.view.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bangkit.rebinmobileapps.R
 import com.bangkit.rebinmobileapps.databinding.ActivityMainBinding
+import com.bangkit.rebinmobileapps.view.detection.DetectionActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -15,10 +17,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val bottomNavigationView: BottomNavigationView = binding.bottomNavigationView
 
         bottomNavigationView.background = null
         bottomNavigationView.menu.getItem(2).isEnabled = false
+
+        binding.fabBtnDetection.setOnClickListener {
+            intent = Intent(this, DetectionActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 
