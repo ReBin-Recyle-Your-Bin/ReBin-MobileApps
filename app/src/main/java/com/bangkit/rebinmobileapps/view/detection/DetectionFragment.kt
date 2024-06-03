f7de364355d3ec624f19a6608ae90f5ddb94eff5
Avrians <avriansyahb10@gmail.com>
2024-06-03 16:18:13 +0700

    [av][update av]

--- app/src/main/java/com/bangkit/rebinmobileapps/view/detection/DetectionFragment.kt
+++ app/src/main/java/com/bangkit/rebinmobileapps/view/detection/DetectionFragment.kt
@@ -12,11 +12,7 @@ import com.bangkit.rebinmobileapps.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DetectionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class DetectionFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
--- app/src/main/java/com/bangkit/rebinmobileapps/view/history/HistoryFragment.kt
+++ app/src/main/java/com/bangkit/rebinmobileapps/view/history/HistoryFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bangkit.rebinmobileapps.R
+import com.bangkit.rebinmobileapps.databinding.FragmentHistoryBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,6 +19,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HistoryFragment : Fragment() {
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
