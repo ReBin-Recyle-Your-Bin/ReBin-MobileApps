e0f468083e51b2f8ba81ccfed4607865e6dae71b
Orry Frasetyo <orrytio12345@gmail.com>
2024-06-06 12:06:05 +0700

    [ory][add model craft and searchcraftAdapter]

---
+++ app/src/main/java/com/bangkit/rebinmobileapps/adapter/SearchCraftAdapter.kt
@@ -0,0 +1,67 @@
+package com.bangkit.rebinmobileapps.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bangkit.rebinmobileapps.data.model.Craft
+import com.bangkit.rebinmobileapps.databinding.ItemCraftBinding
+
+class SearchCraftAdapter : ListAdapter<Craft, SearchCraftAdapter.MyViewHolder>(DIFF_CALLBACK) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = ItemCraftBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val craft = getItem(position)
+        holder.bind(craft)
+    }
+    class MyViewHolder(
+        private val binding: ItemCraftBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(craft: Craft) {
+            binding.tvTitle.text = craft.title
+            binding.tvWriter.text = craft.writer
+            binding.tvDescription.text = craft.description
+            binding.ivImage.setImageResource(craft.imageResource)
+
+        }
+
+    }
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Craft>() {
+            override fun areItemsTheSame(oldItem: Craft, newItem: Craft): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: Craft, newItem: Craft): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/bangkit/rebinmobileapps/data/model/CraftCategory.kt
+++ app/src/main/java/com/bangkit/rebinmobileapps/data/model/CraftCategory.kt
@@ -1,5 +1,8 @@
 package com.bangkit.rebinmobileapps.data.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 data class CraftCategory(val imageResource: Int, val title: String)
 
 data class StoryInpiration(
@@ -7,3 +10,11 @@ data class StoryInpiration(
     val title: String,
     val description: String
 )
+
+@Parcelize
+data class Craft(
+    val imageResource: Int,
+    val title: String,
+    val writer: String,
+    val description: String
+) : Parcelable
