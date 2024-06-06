65efeae8cdaaebf83d97798ee4d449aff6a38219
Orry Frasetyo <orrytio12345@gmail.com>
2024-06-06 16:17:57 +0700

    [ory][add ApiConfig]

--- app/src/main/java/com/bangkit/rebinmobileapps/data/api/ApiConfig.kt
+++ app/src/main/java/com/bangkit/rebinmobileapps/data/api/ApiConfig.kt
@@ -1,4 +1,38 @@
 package com.bangkit.rebinmobileapps.data.api
 
-class ApiConfig {
+import com.bangkit.rebinmobileapps.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiConfig {
+        fun getApiService(): ApiService {
+            val loggingInterceptor =
+                if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+                } else {
+                    HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+                }
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+            //retrofit api dicoding (sementara)
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://story-api.dicoding.dev/v1/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            return retrofit.create(ApiService::class.java)
+        }
+
 }
+
+
+
+
+
+
+
+
