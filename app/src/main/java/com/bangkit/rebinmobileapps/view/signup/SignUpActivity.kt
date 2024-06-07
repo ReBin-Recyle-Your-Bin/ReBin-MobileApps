259beb609dcfc6d9b8fbe9a00531fe4761d50e4a
Orry Frasetyo <orrytio12345@gmail.com>
2024-06-07 10:40:35 +0700

    [ory][menambahkan anotasi di setup action]

--- app/src/main/java/com/bangkit/rebinmobileapps/view/signup/SignUpActivity.kt
+++ app/src/main/java/com/bangkit/rebinmobileapps/view/signup/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.bangkit.rebinmobileapps.view.signup
 
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -53,6 +54,7 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     //setup aksi signup
+    @SuppressLint("SuspiciousIndentation")
     private fun setupAction() {
         binding.signupButton.setOnClickListener {
             val name = binding.nameEditText.text.toString()
