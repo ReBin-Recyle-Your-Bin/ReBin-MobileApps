11f7347a3f07fe3e805c3c3b3eb5dc3f6979c1e0
Orry Frasetyo <orrytio12345@gmail.com>
2024-06-10 18:09:05 +0700

    [ory][menyiapkan room history, dao, repo, viewmodel]

--- app/src/main/java/com/bangkit/rebinmobileapps/di/Injection.kt
+++ app/src/main/java/com/bangkit/rebinmobileapps/di/Injection.kt
@@ -5,8 +5,11 @@ import com.bangkit.rebinmobileapps.data.UserPreferences
 import com.bangkit.rebinmobileapps.data.api.ApiConfig
 import com.bangkit.rebinmobileapps.data.dataStore
 import com.bangkit.rebinmobileapps.data.UserRepository
+import com.bangkit.rebinmobileapps.data.local.room.TrashDetectionDatabase
+import com.bangkit.rebinmobileapps.data.repository.HistoryRepository
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
+import java.util.concurrent.Executors
 
 object Injection {
     fun provideRepository(context: Context): UserRepository {
@@ -17,4 +20,22 @@ object Injection {
         val apiService = ApiConfig.getApiService(user.token)
         return UserRepository.getInstance(apiService, pref)
     }
+
+    fun provideHistoryRepository(context: Context): HistoryRepository {
+        val database = TrashDetectionDatabase.getDatabase(context)
+        val dao = database.historyDao()
+        val appExecutors = Executors.newSingleThreadExecutor()
+        return HistoryRepository.getInstance(dao, appExecutors)
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
