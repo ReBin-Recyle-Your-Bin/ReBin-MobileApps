ba407fcae6d7236c0d2a77fba428c44907aeb43a
Avrians <avriansyahb10@gmail.com>
2024-06-13 17:09:21 +0700

    [av][delete][hapus fungsi service]

--- app/src/main/java/com/bangkit/rebinmobileapps/data/api/ApiConfig.kt
+++ app/src/main/java/com/bangkit/rebinmobileapps/data/api/ApiConfig.kt
@@ -9,36 +9,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object ApiConfig {
-    fun getApiService(token: String): ApiService {
-            val loggingInterceptor =
-                if (BuildConfig.DEBUG) {
-                    HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-                } else {
-                    HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
-                }
-            val authInterceptor = Interceptor { chain ->
-                val req = chain.request()
-                val requestHeaders = req.newBuilder()
-                    .addHeader("Authorization", "Bearer $token")
-                    .build()
-                chain.proceed(requestHeaders)
-            }
-            val client = OkHttpClient.Builder()
-                .addInterceptor(loggingInterceptor)
-                .addInterceptor(authInterceptor)
-                .connectTimeout(30, TimeUnit.SECONDS)
-                .readTimeout(30, TimeUnit.SECONDS)
-                .writeTimeout(30, TimeUnit.SECONDS)
-                .build()
-            
-            val retrofit = Retrofit.Builder()
-                .baseUrl("https://rebin-app.et.r.appspot.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
-                .build()
-            return retrofit.create(ApiService::class.java)
-    }
-
     private fun createRetrofit(baseUrl: String, token: String): Retrofit {
         val loggingInterceptor =
             if (BuildConfig.DEBUG) {
