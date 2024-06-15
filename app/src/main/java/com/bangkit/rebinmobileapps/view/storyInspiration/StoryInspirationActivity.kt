a3b86dbc633a4ea2d2e6d857463e115a16d7d8ca
Avrians <avriansyahb10@gmail.com>
2024-06-15 12:20:18 +0700

    [av][finish][story inspiratin all]

--- app/src/main/java/com/bangkit/rebinmobileapps/view/storyInspiration/StoryInspirationActivity.kt
+++ app/src/main/java/com/bangkit/rebinmobileapps/view/storyInspiration/StoryInspirationActivity.kt
@@ -1,15 +1,64 @@
 package com.bangkit.rebinmobileapps.view.storyInspiration
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bangkit.rebinmobileapps.adapter.StoryInpirationAdapter
+import com.bangkit.rebinmobileapps.data.ResultState
 import com.bangkit.rebinmobileapps.databinding.ActivityStoryInspirationBinding
+import com.bangkit.rebinmobileapps.view.ViewModelFactory
+import com.bangkit.rebinmobileapps.view.main.MainViewModel
+import kotlinx.coroutines.launch
 
 class StoryInspirationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityStoryInspirationBinding
+    private lateinit var storyRecyclerView: RecyclerView
+    private val storyInpirationAdapter = StoryInpirationAdapter()
+
+    private val viewModel by viewModels<MainViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStoryInspirationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupRecyclerView()
+        setupAction()
+    }
+
+    private fun setupRecyclerView() {
+        storyRecyclerView = binding.rvStoryInspirationAll
+        storyRecyclerView.apply {
+            layoutManager = LinearLayoutManager(this@StoryInspirationActivity)
+            adapter = storyInpirationAdapter
+        }
+    }
+
+    private fun setupAction() {
+        lifecycleScope.launch {
+            viewModel.getStoryInspiration().observe(this@StoryInspirationActivity) { story ->
+                when (story) {
+                    is ResultState.Success -> {
+                        binding.progressBar.visibility = View.INVISIBLE
+                        storyInpirationAdapter.submitList(story.data)
+                        binding.rvStoryInspirationAll.adapter = storyInpirationAdapter
+                    }
+                    is ResultState.Loading -> {
+                        binding.progressBar.visibility = View.VISIBLE
+                    }
+                    is ResultState.Error -> {
+                        binding.progressBar.visibility = View.INVISIBLE
+                        Toast.makeText(this@StoryInspirationActivity, "Error: ${story.error}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
     }
 }
