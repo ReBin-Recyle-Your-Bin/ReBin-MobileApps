3a4630245b90475001015b86fcf6126a89becc3d
Orry Frasetyo <orrytio12345@gmail.com>
2024-06-15 23:52:25 +0700

    [ory][hapus import tidak perlu]

--- app/src/main/java/com/bangkit/rebinmobileapps/view/search/SearchViewModel.kt
+++ app/src/main/java/com/bangkit/rebinmobileapps/view/search/SearchViewModel.kt
@@ -1,12 +1,7 @@
 package com.bangkit.rebinmobileapps.view.search
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import androidx.paging.PagingData
-import androidx.paging.cachedIn
 import com.bangkit.rebinmobileapps.data.UserRepository
-import com.bangkit.rebinmobileapps.data.response.SearchCraftItems
 
 class SearchViewModel(private val repository: UserRepository) : ViewModel() {
 
