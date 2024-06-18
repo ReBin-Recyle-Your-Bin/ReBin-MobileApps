1db24268b537a9b2d5fbc72240042a5534858a72
Avrians <avriansyahb10@gmail.com>
2024-06-18 13:47:48 +0700

    [av][add][menambahkan fitur challange]

--- app/src/main/java/com/bangkit/rebinmobileapps/view/fitur/challage/ChallangeActivity.kt
+++ app/src/main/java/com/bangkit/rebinmobileapps/view/fitur/challage/ChallangeActivity.kt
@@ -1,18 +1,62 @@
 package com.bangkit.rebinmobileapps.view.fitur.challage
 
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bangkit.rebinmobileapps.adapter.ChallengeAdapter
+import com.bangkit.rebinmobileapps.data.ResultState
 import com.bangkit.rebinmobileapps.databinding.ActivityChallangeBinding
+import com.bangkit.rebinmobileapps.view.ViewModelFactory
+import com.bangkit.rebinmobileapps.view.main.MainViewModel
 
 class ChallangeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityChallangeBinding
+    private val challengeAdapter = ChallengeAdapter()
+
+    private val viewModel by viewModels<MainViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChallangeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupRecyclerView()
+        setupAction()
 
         binding.tlbChallange.setNavigationOnClickListener {
             onBackPressed()
+        }
+    }
+
+    private fun setupRecyclerView() {
+        val challengeRV = binding.rvChallenge
+        challengeRV.apply {
+            layoutManager = LinearLayoutManager(this@ChallangeActivity)
+            adapter = challengeAdapter
+        }
+    }
+
+    private fun setupAction() {
+        // Setup action
+        viewModel.getChallenge().observe(this@ChallangeActivity) { challenge ->
+            when (challenge) {
+                is ResultState.Success -> {
+                    binding.progressBar.visibility = View.INVISIBLE
+                    binding.tvChallengeEmpty.visibility = View.GONE
+                    challengeAdapter.submitList(challenge.data)
+                    binding.rvChallenge.adapter = challengeAdapter
+                }
+                is ResultState.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                }
+                is ResultState.Error -> {
+                    binding.progressBar.visibility = View.INVISIBLE
+                    binding.tvChallengeEmpty.visibility = View.VISIBLE
+                }
+            }
         }
     }
 }
